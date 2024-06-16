43d5372573f5853b3d2c675df5cb6a6f6407a746
Joao Costa <joaopedrocosta.contato@gmail.com>
2024-06-16 10:54:31 -0300

    Install events and stream packages

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,8 +1,22 @@
 import { Container, AbsoluteCenter, Box, Spacer } from '@chakra-ui/react'
+import { LoaderFunctionArgs, json } from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
 import Logo from '~/components/Logo';
 import SearchInput from '~/components/SearchInput';
 
+export async function loader({context}: LoaderFunctionArgs) {
+  const { GOOGLE_API_KEY } = context.cloudflare.env;
+
+  const kv = await GOOGLE_API_KEY.get("key-teste");
+  
+  return json({ kv });
+}
+
 export default function SearchPage() {
+  const { kv } = useLoaderData<typeof loader>();
+
+  console.log(kv);
+
   return (
     <Container
       minHeight="90vh"
@@ -11,6 +25,8 @@ export default function SearchPage() {
       <AbsoluteCenter w={{ base: "full", md: "unset" }} paddingX="16px">
         <Box w={{ base: "full", md: "576px" }}>
           <Logo />
+          <Spacer marginBottom="44px" />
+          <h1>{kv ?? "Sem kv!!"}</h1>
           <Spacer marginBottom="44px" />
           <SearchInput />
         </Box>
