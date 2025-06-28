0200fbf820e24ddd5006d0f2f505426f0964b3b1
Joao Costa <joaopedrocosta.contato@gmail.com>
2025-06-28 02:07:24 -0300

    Add netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,7 @@ import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
+    remix(),
     tsconfigPaths(),
     netlify(),
   ],
